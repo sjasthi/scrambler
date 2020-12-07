aa28f2bd76a61809ad15c431e8bcf7775de851d4
Jasthi <srj@splm.siemens.com>
2020-12-07 10:44:02 -0600

    Update deleteWordset.php

--- db/deleteWordset.php
+++ db/deleteWordset.php
@@ -1,17 +1,19 @@
-<?php 
+<?php
 
-    if(session_id() == '' || !isset($_SESSION)){
+if (session_id() == '' || !isset($_SESSION))
+{
     session_start();
-    }
+}
 
-    $nav_selected = "ADMIN";
-    $left_buttons = "NO";
-    $left_selected = "WORDSETS";
+$nav_selected = "ADMIN";
+$left_buttons = "NO";
+$left_selected = "WORDSETS";
 
-    include("../includes/innerNav.php");
+include ("../includes/innerNav.php");
 
-    if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
-      ?>
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false)
+{
+?>
       
       
         <div class="right-content">
@@ -22,9 +24,11 @@
           </div>
         </div>
       
-      <?php
-    } else if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
-      ?>
+<?php
+}
+else if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin')
+{
+?>
       
       
         <div class="right-content">
@@ -36,37 +40,45 @@
         </div>
       
       <?php
-    } else {
+}
+else
+{
 
 ?>
 <style>#title {text-align: center; color: darkgoldenrod;}</style>
+
 <?php
+    if (isset($_POST['ident']))
+    {
 
-if (isset($_POST['ident'])){
+        $id = mysqli_real_escape_string($db, $_POST['ident']);
 
-    $id = mysqli_real_escape_string($db, $_POST['ident']);
-    
-    $sql = "SELECT * from word_sets_meta WHERE set_id = '$id'";
+        $sql = "SELECT * from word_sets_meta WHERE set_id = '$id'";
 
-    $result = mysqli_query($db, $sql);
+        $result = mysqli_query($db, $sql);
 
-    if($result) {
-        if($result->num_rows > 0){
-            while($row = $result->fetch_assoc()){
-                $wordId = $row['word_id'];
-                $sql = "DELETE FROM word_sets
+        if ($result)
+        {
+            if ($result->num_rows > 0)
+            {
+                while ($row = $result->fetch_assoc())
+                {
+                    $wordId = $row['word_id'];
+                    $sql = "DELETE FROM word_sets
                         WHERE word_id = '$wordId'";
-                mysqli_query($db, $sql);
+                    mysqli_query($db, $sql);
+                }
             }
         }
-    }
 
-    $sql = "DELETE FROM word_sets_meta
+        $sql = "DELETE FROM word_sets_meta
             WHERE set_id = '$id'";
 
-    mysqli_query($db, $sql);
+        mysqli_query($db, $sql);
 
-    header('location: ../otherPages/admin_wordsets.php?edited=deleted');
+        header('location: ../otherPages/admin_wordsets.php?edited=deleted');
+    }
+
 }
 
 ?>
